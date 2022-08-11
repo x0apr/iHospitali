fac90dd03bd598356cf3b13479ea117bb6bc8643
aprrrnl <ampurirer@tutanota.com>
2022-08-11 08:52:15 +0300

    v1

--- library.php
+++ library.php
@@ -5,11 +5,25 @@
 ?>
 
 <?php
+/**
+ for local host
+
     $servername='localhost';
     $username='root';
     $password='devpass';
     $connection = new mysqli($servername, $username, $password, 'ihospitali');
-
+ */
+ 
+ //for heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+ 
     $error_flag = 0;
     $result;
     if ($connection->connect_error) {
